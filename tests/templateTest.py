f5966293cf95d86e461c6603ff3bc9a049afc387
rzhade3 <rzhade@gatech.edu>
2018-03-22 22:29:31 -0700

    Added Travis CI support and one unit test

---
+++ tests/templateTest.py
@@ -0,0 +1,18 @@
+from pathlib import Path
+import os
+import requests
+
+def test_HTML_validator():
+	rootDir = os.path.dirname(os.path.abspath(__file__))
+	pathlist = Path(rootDir + '/../api/templates')
+	for file in list(pathlist.glob('*.html')):
+		with file.open() as f: 
+			r = requests.post('https://validator.w3.org/nu/', 
+                    data=f.read(), 
+                    params={'out': 'json'}, 
+                    headers={'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.101 Safari/537.36', 
+                    'Content-Type': 'text/html; charset=UTF-8'})
+			assert(len(r.json()['messages']) == 0)
+
+# if __name__ == '__main__':
+# 	test_HTML_validator()
